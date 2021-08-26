7d66a8ff307d2da93a0a585e831f0bdc36faac0e
Lukasz Szczepaniak <lukasz.webdev7@gmail.com>
2021-08-26 21:22:38 +0200

    check solution with messenger

--- components/contactPage/messengerF.js
+++ components/contactPage/messengerF.js
@@ -2,10 +2,10 @@ import MessengerCustomerChat from 'react-messenger-customer-chat';
 
 function FacebookMessenger() {
 	const secret_key = process.env.messenger_key;
-	const page_Id = process.env.pageId;
+	// const page_Id = process.env.pageId;
 	return (
 		<div>
-			<MessengerCustomerChat pageId={page_Id} appId={secret_key} />
+			<MessengerCustomerChat pageId="102564995007262" appId={secret_key} />
 		</div>
 	);
 }
