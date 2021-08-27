7a0fa644930bf2519747b5e564fba958e75d4af6
Lukasz Szczepaniak <lukasz.webdev7@gmail.com>
2021-08-27 17:10:12 +0200

    check messenger chat

--- components/contactPage/messengerF.js
+++ components/contactPage/messengerF.js
@@ -5,7 +5,11 @@ function FacebookMessenger() {
 	// const page_Id = process.env.pageId;
 	return (
 		<div>
-			<MessengerCustomerChat pageId="102564995007262" appId={secret_key} />
+			<MessengerCustomerChat
+				pageId="102564995007262"
+				appId={secret_key}
+				htmlRef="https://connect.facebook.net/pl_PL/sdk/xfbml.customerchat.js"
+			/>
 		</div>
 	);
 }
